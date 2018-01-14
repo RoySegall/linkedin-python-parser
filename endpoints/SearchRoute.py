3dbaa6e6791451a567860fb4d7d38eb5f058c7c6
Roy Segall <roy.segall@gizra.com>
2018-01-14 22:59:30 +0200

    Start to work on the search endpoint.

--- endpoints/SearchRoute.py
+++ endpoints/SearchRoute.py
@@ -10,20 +10,38 @@ class SearchRoute(BaseRoute):
 
     def Routes(self):
         return [
-            Route('/', 'GET', self.searchGet),
-            Route('/', 'POST', self.searchPost),
-            Route('/', 'PATCH', self.searchPatch),
-            Route('/', 'DELETE', self.searchDelete),
+            Route('/', 'POST', self.search),
         ]
 
-    def searchGet(self):
+    def search(self):
+        """
+        Searching for a user in the DB.
+
+        :return:
+        """
+        # Get the search text.
+
+        # Search in the text in the name, title, position, summary.
+
+        score = self.calculate_score("", {})
+
         return {}
 
-    def searchPost(self):
-        return {}
+    def calculate_score(self, text, user_object):
+        """
+        Calculating the score for a user name.
 
-    def searchPatch(self):
-        return {}
+        If the text exists only in the title or the position current position - the score is 1.
 
-    def searchDelete(self):
-        return {}
+        If the text appears in the description of the user and the skills that's mean the user is matching for search
+        based on a tech. In that case the person is a good match and the score will be the number or endorsements.
+
+        :param text:
+            The text the user searched for.
+        :param user_object:
+            The user object.
+
+        :return:
+            A score based on the object.
+        """
+        return 1
