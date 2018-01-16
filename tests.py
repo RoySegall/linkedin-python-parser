30729892004b9d5be0e822d47caf79b47ac60088
Roy Segall <roy.segall@gizra.com>
2018-01-16 10:05:03 +0200

    Tests.

---
+++ tests.py
@@ -0,0 +1,14 @@
+from apistar.test import TestClient
+from app import app
+
+
+def test_http_request():
+    """
+    Testing a view, using the test client.
+    """
+
+    # First clean any data from the DB.
+
+    # Create three elements.
+
+    # Start to run tests against the API.
